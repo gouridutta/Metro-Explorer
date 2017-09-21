a900051db41893baf2a0905f845feac4dfaf216e
timtraversy <timtrav@gmail.com>
2017-09-21 16:46:05 -0400

    Added four main activities in architecture

---
+++ app/src/main/java/com/coursegnome/metroexplorer/LandmarksActivity.kt
@@ -0,0 +1,12 @@
+package com.coursegnome.metroexplorer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LandmarksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landmarks)
+    }
+}
---
+++ app/src/main/java/com/coursegnome/metroexplorer/MetroStationsActivity.kt
@@ -0,0 +1,12 @@
+package com.coursegnome.metroexplorer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MetroStationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metro_stations)
+    }
+}
