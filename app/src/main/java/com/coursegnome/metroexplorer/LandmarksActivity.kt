ad29ceb8732d1f9d955f89401b258e57542ea10e
timtraversy <timtrav@gmail.com>
2017-09-25 11:41:41 -0400

    Added Recycler View and Adapter

--- app/src/main/java/com/coursegnome/metroexplorer/LandmarksActivity.kt
+++ app/src/main/java/com/coursegnome/metroexplorer/LandmarksActivity.kt
@@ -2,11 +2,20 @@ package com.coursegnome.metroexplorer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.StaggeredGridLayoutManager
 
 class LandmarksActivity : AppCompatActivity() {
+
+    lateinit private var staggeredLayoutManager : StaggeredGridLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landmarks)
+
+        staggeredLayoutManager = StaggeredGridLayoutManager(1,
+                StaggeredGridLayoutManager.VERTICAL)
+        list.layoutManager = staggeredLayoutManager
+//        list.layoutManager = staggeredLayoutManager
+
     }
 }
